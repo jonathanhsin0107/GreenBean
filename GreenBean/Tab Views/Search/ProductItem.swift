4e36806d78a96a4f364ab50e42bcab613c7a265d
KoolBushido <jonathanhsin0107@gmail.com>
2025-03-13 22:44:17 -0400

    Fixed productItem to use pre-created product classes

--- GreenBean/Tab Views/Search/ProductItem.swift
+++ GreenBean/Tab Views/Search/ProductItem.swift
@@ -2,101 +2,39 @@
 import SwiftUI
 
 struct ProductItem: View {
-    let product: Any
-    
-    // Computed properties to safely access fields
-    private var productName: String {
-        if let p = product as? Product {
-            return p.productName
-        } else if let p = product as? ProductsData {
-            return p.productName
-        }
-        return ""
-    }
-    
-    private var price: String {
-        if let p = product as? Product {
-            return p.price
-        } else if let p = product as? ProductsData {
-            return p.price
-        }
-        return ""
-    }
-    
-    private var size: String {
-        if let p = product as? Product {
-            return p.size
-        } else if let p = product as? ProductsData {
-            return p.size
-        }
-        return ""
-    }
-    
-    private var store: String {
-        if let p = product as? Product {
-            return p.store
-        } else if let p = product as? ProductsData {
-            return p.store
-        }
-        return ""
-    }
-    
-    private var imageLink: String {
-        if let p = product as? Product {
-            return p.imageLink
-        } else if let p = product as? ProductsData {
-            return p.imageLink
-        }
-        return ""
-    }
+    let product: Product
     
     var body: some View {
         HStack {
-            AsyncImage(url: URL(string: imageLink)) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0)
-                } else if phase.error != nil {
-                    Image(systemName: "photo")
-                        .frame(width: 100.0, height: 100.0)
-                } else {
-                    ProgressView()
-                        .frame(width: 100.0, height: 100.0)
-                }
-            }
+            getImageFromUrl(url: product.imageLink, defaultFilename: "ImageUnavailable")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100.0)
             
             VStack(alignment: .leading) {
-                Text(productName)
-                    .lineLimit(2)
-                
+                Text(product.productName)
                 HStack {
                     Image(systemName: "tag")
                         .imageScale(.small)
                         .font(Font.title.weight(.thin))
-                    Text(price.hasPrefix("$") ? price : "$\(price)")
+                    Text(product.price.hasPrefix("$") ? product.price : "$\(product.price)")
                 }
-                
                 HStack {
                     Image(systemName: "shippingbox")
                         .imageScale(.small)
                         .font(Font.title.weight(.thin))
-                    Text(size)
+                    Text(product.size)
                 }
-                
                 HStack {
                     Image(systemName: "cart")
                         .imageScale(.small)
                         .font(Font.title.weight(.thin))
-                    Text(store)
+                    Text(product.store)
                 }
             }
+            // Set font and size for the whole VStack content
             .font(.system(size: 14))
-        }
+            
+        }   // End of HStack
     }
-}
-
-#Preview {
-    ProductItem(product: productStructList[0])
 }
