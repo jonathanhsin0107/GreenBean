884e7fbf9311241ba6eb47ea28089bd2448f42cf
Baibhav Nepal <baibhav.xavier@gmail.com>
2025-03-16 15:28:00 -0400

    Test Rewards

--- GreenBean/Tab Views/Rewards/Rewards.swift
+++ GreenBean/Tab Views/Rewards/Rewards.swift
@@ -1,19 +1,6 @@
 //
 //  Rewards.swift
 //  GreenBean
-//
-//  Created by Jonathan on 2/25/25.
-//  Copyright © 2025 Jonathan Hsin. All rights reserved.
-//
 
 import SwiftUI
 
-struct Rewards: View {
-    var body: some View {
-        Text("This is the rewards view!")
-    }
-}
-
-#Preview {
-    Rewards()
-}
