cf9317d6d166149de4d201ec7cffb6adba451fd8
Baibhav Nepal <baibhav.xavier@gmail.com>
2025-03-16 16:21:25 -0400

    Updated rewards.swift

--- GreenBean/Tab Views/Rewards/Rewards.swift
+++ GreenBean/Tab Views/Rewards/Rewards.swift
@@ -1,6 +1,43 @@
-//
-//  Rewards.swift
-//  GreenBean
+// Program that creates the UI/preview of the rewards page. 
+// Also integrates the reward points algorithm to ensure that reward points
+// are calculated accurately.
+
+// CS 4644 (Creative Computing Capstone)
+// Team 1 (GreenBean)
 
 import SwiftUI
 
+struct Rewards: View {
+    @StateObject private var viewModel = RewardsAlgorithm()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Your Reward Points: \(viewModel.totalPoints)")
+                .font(.title)
+            
+            if let badge = viewModel.currentBadge {
+                Text("Current Badge: \(badge)")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.green.opacity(0.3))
+                    .cornerRadius(10)
+            }
+
+            Button("Add Points") {
+                ScanReceipt()
+            }
+            .buttonStyle(.borderedProminent)
+            
+            // Button("Reset Rewards") {
+            //     viewModel.resetRewards()
+            // }
+            // .buttonStyle(.bordered)
+            // .foregroundColor(.green)
+        }
+        // .padding()
+    }
+}
+
+#Preview {
+  Rewards()
+}
