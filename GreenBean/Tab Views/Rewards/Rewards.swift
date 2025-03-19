8081e1469ae83e3c030a2991f0b2e4e9c6a5430d
Baibhav Nepal <baibhav.xavier@gmail.com>
2025-03-18 23:06:27 -0400

    Badge images integrated

--- GreenBean/Tab Views/Rewards/Rewards.swift
+++ GreenBean/Tab Views/Rewards/Rewards.swift
@@ -19,32 +19,44 @@ struct Rewards: View {
                 RoundedRectangle(cornerRadius: 16)
                     .fill(Color(.systemGray6))
                     .shadow(color: .gray.opacity(0.4), radius: 4, x: 2, y: 2)
+                //  .frame(height: 80)
                 Text("Total Points: \(rewardsAlgo.totalPoints)")
                     .font(.title2)
                     .padding()
             }
+            //.padding(.horizontal)
 
             ZStack {
                 RoundedRectangle(cornerRadius: 16)
                     .fill(Color(.systemGray6))
                     .shadow(color: .gray.opacity(0.4), radius: 4, x: 2, y: 2)
-                
-                if let badge = rewardsAlgo.currentBadge {
-                    Text("Badge 🏅: \(badge)")
-                        .font(.title3)
-                        .foregroundColor(.green)
-                        .bold()
-                } else {
-                    Text("No badges earned yet!")
-                        .font(.title2)
-//                        .foregroundColor(.gray)
+                //  .frame(height: 100)
+            
+                HStack {    
+                    if let badge = rewardsAlgo.currentBadge, let badgeImage = getBadgeImage(for: badge) {
+                        Image(badgeImage)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        Text("Badge 🏅: \(badge)")
+                            .font(.title3)
+                            .foregroundColor(.green)
+                            .bold()
+                    } else {
+                        Text("No badges earned yet!")
+                            .font(.title2)
+                    }
                 }
+                .padding()
             }
+            .padding(.horizontal)
 
             ZStack {
                 NavigationLink(destination: Scan()) {
                     Text("Scan Purchase to Earn Points")
                         .padding()
+                    //  .frame(maxWidth: .infinity)
                         .foregroundColor(.white)
                         .background(Color.blue)
                         .cornerRadius(8)
@@ -52,6 +64,15 @@ struct Rewards: View {
             }
         }
         .padding()
+    }
+
+    func getBadgeImage(for badge: String) -> String? {
+        switch badge {
+            case "Planet Caretaker": return "planet_caretaker_picture"
+            case "Ecological Hero": return "ecological_hero_picture"
+            case "Sustainability Champion": return "sustainability_champion_picture"
+            default: return nil
+        }
     }
 }
             
